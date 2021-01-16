b4aeb62af850132c93ce5c720e8ee1a85c60aa3c
Iniguezdavid <iniguezdavidalejandro@gmail.com>
2021-01-16 12:33:49 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando en el puerto 3000');
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
